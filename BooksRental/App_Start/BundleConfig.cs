85226fc61529f1df8020a46b21d3539bb4c15461
Sendres <sfec03@gmail.com>
2015-10-14 13:23:42 -0600

    Modal added to code, and datatable of comments also

--- BooksRental/App_Start/BundleConfig.cs
+++ BooksRental/App_Start/BundleConfig.cs
@@ -9,10 +9,15 @@ namespace BooksRental
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/Main/Global.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datatable").Include(
+                        "~/Content/Plugins/datatables/media/js/jquery.dataTables.min.js",
+                        "~/Content/Plugins/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -26,6 +31,15 @@ namespace BooksRental
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+
+            bundles.Add(new StyleBundle("~/Content/Datatable/css").Include(
+                      "~/Content/Plugins/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css",
+                      "~/Content/Plugins//datatables-responsive/css/dataTables.responsive.css"));
+            
+
+
+
         }
     }
 }
---
+++ BooksRental/Scripts/Main/Global.js
@@ -0,0 +1,13 @@
+﻿function validateFileType(fileId, ErrorLblId, submitId) {
+    var ext = $('#' + fileId).val().split('.').pop().toLowerCase();
+    var is = $('#' + fileId).val();
+    if ($.inArray(ext, ['png', 'jpg', 'jpeg']) == -1 && is != "") {
+        $('#' + ErrorLblId).text('The image can be only png, jpg or jpeg');
+        $('#' + ErrorLblId).show();
+        $('#' + submitId).prop('disabled', true);
+    } else {
+        $('#'+submitId).prop('disabled', false);
+        $('#' + ErrorLblId).hide();
+    }
+}
+
