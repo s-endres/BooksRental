e8e73abef1fbac79a10a807469b97ac7b08af3cc
Sendres <sfec03@gmail.com>
2015-10-20 08:04:15 -0600

    Doble Model Example done

---
+++ BooksRental/Controllers/DobleModelController.cs
@@ -0,0 +1,21 @@
+﻿using BooksRental.POCOs;
+using BooksRental.Repositories;
+using System.Web.Mvc;
+
+namespace BooksRental.Controllers
+{
+    public class DobleModelController : Controller
+    {
+        private AccountRepository AcRepo = new AccountRepository(GlobalVariables.ConnectionString);
+        private BookGenderRepository BGRepo = new BookGenderRepository(GlobalVariables.ConnectionString);
+
+        // GET: DobleModel
+        public ActionResult Index()
+        {
+            ViewData["AccountTypes"] = AcRepo.GetAccountTypes();
+            ViewData["BookGenders"] = BGRepo.GetAll();
+
+            return View();
+        }
+    }
+}
