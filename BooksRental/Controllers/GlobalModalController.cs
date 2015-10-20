f018311bf7d6f3ae4a42d2da7887b5ee2ce958e0
Sendres <sfec03@gmail.com>
2015-10-20 09:48:18 -0600

    Global Modal added

---
+++ BooksRental/Controllers/GlobalModalController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace BooksRental.Controllers
+{
+    public class GlobalModalController : Controller
+    {
+        [HttpPost]
+        public ActionResult Moblal(string PageCalled)
+        {
+            ViewBag.PageCalled = PageCalled;
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult Moblal()
+        {
+            return PartialView();
+        }
+    }
+}
